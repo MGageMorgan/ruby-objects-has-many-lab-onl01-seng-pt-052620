476bfca6bff7ba0df7a1d2c9abce5174a3852f75
Gage Morgan <gage@gages.blog>
2020-06-28 20:16:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
-class Song 
+class Song
   attr_accessor :name, :artist
   @@all = []  def initialize(name)
-    @name = name 
-    @@all << self 
-  end   
+    @name = name
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end   
+  def self.all
+    @@all
+  end
 
   def artist_name
     artist.name if artist
-  end 
-end 
+  end
+end
