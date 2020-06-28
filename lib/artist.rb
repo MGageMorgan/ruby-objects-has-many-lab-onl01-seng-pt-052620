ae0ba3d80b96125a998e6bb90dce3fb6e67fc295
Gage Morgan <gage@gages.blog>
2020-06-28 20:15:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,30 @@
-require 'pry'
-
-class Artist
-  attr_accessor :name, :songs
-
+class Artist 
+  attr_accessor :name 
   @@all = []
-
+   
   def initialize(name)
-    @name = name
+   @name = name 
+   @@all << self 
   end
-
-  def songs
-    @@all
-  end
+    
+  def self.all 
+   @@all 
+  end  
 
   def add_song(song)
-    @@all << song
-    song.artist = self
-  end
+   song.artist = self 
+  end  
 
   def add_song_by_name(name)
-    new_song = Song.new(name)
-    new_song.artist = self
-  end
+   song = Song.new(name)
+   song.artist = self 
+  end  
 
   def songs
-    Song.all.select {|song| song.artist == self}
-  end
+   Song.all.select {|song| song.artist == self}
+  end  
 
-  def Artist.song_count
-    Song.all.count()
-  end
-end
+  def self.song_count
+   Song.all.count 
+  end 
+end 
